4fc5455870d7f0511dfcc05f807519c469d00ce8
Magnus Lindhe <magnus.lindhe@emway.se>
2020-12-11 11:26:37 +0100

    Initial sample

---
+++ ModernWpfUIValidation/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace ModernWpfUIValidation
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        private string shouldNotBeEmpty = string.Empty;
+        public string ShouldNotBeEmpty
+        {
+            get => shouldNotBeEmpty;
+            set
+            {
+                shouldNotBeEmpty = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ShouldNotBeEmpty)));
+            }
+        }
+    }
+}
---
+++ ModernWpfUIValidation/NotEmptyValidationRule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Controls;
+
+namespace ModernWpfUIValidation
+{
+    public class NotEmptyValidationRule : ValidationRule
+    {
+        public string Message { get; set; }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (string.IsNullOrWhiteSpace(value?.ToString()))
+            {
+                return new ValidationResult(false, Message ?? "A value is required");
+            }
+            return ValidationResult.ValidResult;
+        }
+    }
+}
